8a57d325a6e0d6c06a9c12e93fa02eef2a2f1179
Hrajto1 <135441727+Hrajto1@users.noreply.github.com>
2025-04-27 19:58:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -78,5 +78,6 @@ def lucky():
 def static_files(filename):
     return send_from_directory(app._static_folder, filename)
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+if __name__=="__main__":
+    # port vezme Railway z env var PORT
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
